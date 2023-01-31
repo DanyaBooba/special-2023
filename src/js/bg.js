a662c42c2f4d2e256c52ff32a43f10701354aff4
checkthesound <danil.dybko@gmail.com>
2023-02-01 01:38:34 +0300

    get change bg image and color

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,3 +1,24 @@
 function ChangeBackground() {
-    console.log('change background');
+    let info = localStorage.getItem('bg-color');
+    if (info === 'color') {
+        localStorage.removeItem('bg-color');
+    }
+    else {
+        localStorage.setItem('bg-color', 'color');
+    }
+
+    GetColor();
 }
+
+function GetColor() {
+    let info = localStorage.getItem('bg-color');
+
+    if (info === 'color') {
+        document.body.classList.add('get-body--color');
+    }
+    else {
+        document.body.classList.remove('get-body--color');
+    }
+}
+
+GetColor();
