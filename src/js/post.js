7c95636a898e18c94c2b33f226002987b5fa1ad3
checkthesound <danil.dybko@gmail.com>
2023-02-18 17:50:16 +0300

    get change font size

--- src/js/post.js
+++ src/js/post.js
@@ -28,16 +28,42 @@ function PostTextDefault() {
 }
 
 function SetFontSize(num) {
+	let block = document.getElementById("post--main");
+
 	if (num == 2) {
 		console.log("2");
+		block.classList.remove("post--font-size-u1");
+		block.classList.remove("post--font-size-u2");
+		block.classList.remove("post--font-size-d1");
+		block.classList.remove("post--font-size-d2");
+		block.classList.add("post--font-size-u2");
 	} else if (num == 1) {
 		console.log("1");
+		block.classList.remove("post--font-size-u1");
+		block.classList.remove("post--font-size-u2");
+		block.classList.remove("post--font-size-d1");
+		block.classList.remove("post--font-size-d2");
+		block.classList.add("post--font-size-u1");
 	} else if (num == -1) {
 		console.log("-1");
+		block.classList.remove("post--font-size-u1");
+		block.classList.remove("post--font-size-u2");
+		block.classList.remove("post--font-size-d1");
+		block.classList.remove("post--font-size-d2");
+		block.classList.add("post--font-size-d1");
 	} else if (num == -2) {
 		console.log("-2");
+		block.classList.remove("post--font-size-u1");
+		block.classList.remove("post--font-size-u2");
+		block.classList.remove("post--font-size-d1");
+		block.classList.remove("post--font-size-d2");
+		block.classList.add("post--font-size-d2");
 	} else {
 		console.log("default");
+		block.classList.remove("post--font-size-u1");
+		block.classList.remove("post--font-size-u2");
+		block.classList.remove("post--font-size-d1");
+		block.classList.remove("post--font-size-d2");
 	}
 }
 
