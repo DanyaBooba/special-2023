57281b1712b33ee5a9bcbff87bfff7d2f52dccab
checkthesound <danil.dybko@gmail.com>
2023-03-25 20:00:33 +0300

    work at site

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,24 +1,25 @@
 function ChangeBackground() {
-    let info = localStorage.getItem('bg-color');
-    if (info === 'color') {
-        localStorage.removeItem('bg-color');
-    }
-    else {
-        localStorage.setItem('bg-color', 'color');
-    }
+	let info = localStorage.getItem("bg-color");
+	if (info === "color") {
+		localStorage.removeItem("bg-color");
+	} else {
+		localStorage.setItem("bg-color", "color");
+	}
 
-    GetColor();
+	ActiveBackground();
 }
 
-function GetColor() {
-    let info = localStorage.getItem('bg-color');
+function ActiveBackground() {
+	let info = localStorage.getItem("bg-color");
+	let mobileMenu = document.getElementById("mobileMenu");
 
-    if (info === 'color') {
-        document.body.classList.add('get-body--color');
-    }
-    else {
-        document.body.classList.remove('get-body--color');
-    }
+	if (info === "color") {
+		document.body.classList.add("get-body--color");
+		mobileMenu.classList.add("get-body--color");
+	} else {
+		document.body.classList.remove("get-body--color");
+		mobileMenu.classList.remove("get-body--color");
+	}
 }
 
-GetColor();
+ActiveBackground();
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,81 +1,77 @@
-let light = document.getElementById('stylesheetlight');
-let dark = document.getElementById('stylesheetdark');
+let light = document.getElementById("stylesheetlight");
+let dark = document.getElementById("stylesheetdark");
 
 function ChangeStatusAuto() {
-    localStorage.removeItem('color-theme');
-    light.media = "(prefers-color-scheme: light)";
-    dark.media = "(prefers-color-scheme: dark)";
+	localStorage.removeItem("color-theme");
+	light.media = "(prefers-color-scheme: light)";
+	dark.media = "(prefers-color-scheme: dark)";
 }
 
 function ChangeStatusLight() {
-    localStorage.setItem('color-theme', 'light');
-    light.media = "all";
-    dark.media = "not all";
+	localStorage.setItem("color-theme", "light");
+	light.media = "all";
+	dark.media = "not all";
 }
 
 function ChangeStatusDark() {
-    localStorage.setItem('color-theme', 'dark');
-    light.media = "not all";
-    dark.media = "all";
-}
-
-function ChangeTheme() {
-    if (localStorage.getItem('color-theme') === 'dark') {
-        ChangeStatusLight();
-    }
-    else if (localStorage.getItem('color-theme') === 'light') {
-        ChangeStatusAuto();
-    }
-    else {
-        ChangeStatusDark();
-    }
-}
-
-function ChangeThemeButton() {
-    if (localStorage.getItem('color-theme') === 'dark') {
-        ChangeStatusLight();
-        GetButtonLight();
-    }
-    else if (localStorage.getItem('color-theme') === 'light') {
-        ChangeStatusAuto();
-        GetButtonAuto();
-    }
-    else {
-        ChangeStatusDark();
-        GetButtonDark();
-    }
+	localStorage.setItem("color-theme", "dark");
+	light.media = "not all";
+	dark.media = "all";
 }
 
 function GetButtonDark() {
-    let buttonsvg = document.getElementById('changethemesvg');
-    if (buttonsvg !== null) {
-        buttonsvg.href.baseVal = "/img/icons/theme.svg#dark";
-    }
+	let buttonsvg = document.getElementById("changethemesvg");
+	if (buttonsvg !== null) {
+		buttonsvg.href.baseVal = "/img/icons/theme.svg#dark";
+	}
 }
 
 function GetButtonAuto() {
-    let buttonsvg = document.getElementById('changethemesvg');
-    if (buttonsvg !== null) {
-        buttonsvg.href.baseVal = "/img/icons/theme.svg#auto";
-    }
+	let buttonsvg = document.getElementById("changethemesvg");
+	if (buttonsvg !== null) {
+		buttonsvg.href.baseVal = "/img/icons/theme.svg#auto";
+	}
 }
 
 function GetButtonLight() {
-    let buttonsvg = document.getElementById('changethemesvg');
-    if (buttonsvg !== null) {
-        buttonsvg.href.baseVal = "/img/icons/theme.svg#light";
-    }
+	let buttonsvg = document.getElementById("changethemesvg");
+	if (buttonsvg !== null) {
+		buttonsvg.href.baseVal = "/img/icons/theme.svg#light";
+	}
 }
 
-if (localStorage.getItem('color-theme') === 'dark') {
-    ChangeStatusDark();
-    GetButtonDark();
+// MOBILE BUTTON
+function ChangeTheme() {
+	if (localStorage.getItem("color-theme") === "dark") {
+		ChangeStatusLight();
+	} else if (localStorage.getItem("color-theme") === "light") {
+		ChangeStatusDark();
+	} else {
+		ChangeStatusAuto();
+	}
 }
-else if (localStorage.getItem('color-theme') === 'light') {
-    ChangeStatusLight();
-    GetButtonLight();
+
+// PC BUTTON
+function ChangeThemeButton() {
+	if (localStorage.getItem("color-theme") === "dark") {
+		ChangeStatusLight();
+		GetButtonLight();
+	} else if (localStorage.getItem("color-theme") === "light") {
+		ChangeStatusDark();
+		GetButtonDark();
+	} else {
+		ChangeStatusAuto();
+		GetButtonAuto();
+	}
 }
-else {
-    ChangeStatusAuto();
-    GetButtonAuto();
+
+if (localStorage.getItem("color-theme") === "dark") {
+	ChangeStatusDark();
+	GetButtonDark();
+} else if (localStorage.getItem("color-theme") === "light") {
+	ChangeStatusLight();
+	GetButtonLight();
+} else {
+	ChangeStatusAuto();
+	GetButtonAuto();
 }
