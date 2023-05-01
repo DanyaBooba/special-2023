d35026a8a637538c39435afd0ac7b1248f404893
Daniil Dybka <creagoooff@gmail.com>
2023-05-01 20:38:13 +0300

    work at archive

--- src/js/copylink.js
+++ src/js/copylink.js
@@ -1,5 +1,30 @@
 function CopyLink() {
-	let text = "https://news.dybka.ru" + window.location.pathname;
-
+	let text = SpecialURL();
 	navigator.clipboard.writeText(text);
+}
+
+function LinkToVK() {
+	window.open("https://vk.com/share.php?url=" + SpecialURL(), "_blank");
+}
+
+function LinkToTelegram() {
+	window.open(
+		"https://telegram.me/share/url?url=" +
+			SpecialURL() +
+			"&text=" +
+			document.title,
+		"_blank"
+	);
+}
+
+function LinkToOK() {
+	window.open(
+		"https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=" +
+			SpecialURL(),
+		"_blank"
+	);
+}
+
+function SpecialURL() {
+	return "https://news.dybka.ru" + window.location.pathname;
 }
